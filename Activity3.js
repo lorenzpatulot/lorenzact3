6b76eb0be1c181e9e185907b5b42913ba1cde14b
lorenzpatulot <98679976+lorenzpatulot@users.noreply.github.com>
2022-02-23 14:30:39 +0800

    Add files via upload

---
+++ Activity3.js
@@ -0,0 +1,7 @@
+// store input numbers
+const num1=parseInt(prompt( 'enter the first number' ));
+const num2=parseInt(prompt( 'enter the second number number' ));
+// add two numbers
+const sum= num1 + num2;
+// display the sum
+document.write(' the sum of ${num1} and ${num2} is ${sum}');
